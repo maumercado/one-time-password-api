1106db760e9f650399582e0155cb27296b539c15
Mauricio Mercado <mau@maumercado.com>
2018-01-30 18:31:24 -0800

    Add verification of phone endpoint to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const createUser = require("./create_user");
 const requestOneTimePassword = require("./request_one_time_password");
+const verifyOneTimePassword = require("./verify_one_time_password");
 
 // Set up the service client in order to access data store
 const serviceAccount = require("./service_account.json");
@@ -14,6 +15,11 @@ admin.initializeApp({
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.createUser = functions.https.onRequest(createUser);
+
 exports.requestOneTimePassword = functions.https.onRequest(
     requestOneTimePassword
+);
+
+exports.verifyOneTimePassword = functions.https.onRequest(
+    verifyOneTimePassword
 );
---
+++ functions/verify_one_time_password.js
@@ -0,0 +1,37 @@
+const admin = require("firebase-admin");
+
+module.exports = function(req, res) {
+    if (!req.body.phone || !req.body.code) {
+        return res
+            .status(422)
+            .send({ error: "Phone and code must be provided" });
+    }
+
+    const phone = String(req.body.phone).replace(/[^\d]/g, "");
+    const code = parseInt(req.body.code);
+
+    return admin
+        .auth()
+        .getUser(phone)
+        .then(() => {
+            const ref = admin.database().ref(`users/${phone}`);
+            return ref.once("value", snapshot => {
+                const user = snapshot.val();
+
+                if (user.code !== code || !user.codeValid) {
+                    return res.status(422).send({ error: "Code not valid" });
+                }
+
+                return ref.update({ codeValid: false });
+            });
+        })
+        .then(() => {
+            return admin.auth().createCustomToken(phone);
+        })
+        .then(token => {
+            return res.send({ token });
+        })
+        .catch(err => {
+            return res.status(422).send(err);
+        });
+};
